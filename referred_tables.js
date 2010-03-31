bd8b1fd3996c1576982c8d10407e1ee00f9f24a0
Dmitry <dmitry@gmail.com>
2010-03-31 16:55:10 +0400

    Display links to foreign tables at "editing row" form. Now all data pages are fully hyperlinked using foreign keys.

--- referred_tables.js
+++ referred_tables.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('.ajax_referrers').click(function() {
+	function trigger() {
 		if (this.popup) {
 			$(this.popup).remove();
 			delete this.popup;
@@ -9,11 +9,20 @@ $(document).ready(function() {
 		var hiddens = $('input', this);
 		var lines = [];
 		for (var i = 0; i < hiddens.length; i++) {
-			lines.push("<a href='" + hiddens[i].value + "'><div class='refererrer'>" + hiddens[i].name + "<span class='" + hiddens[i].name + "'></span></div></a>");
+			var path = hiddens[i].name.split(".");
+			lines.push("<a href='" + hiddens[i].value + "'><div class='refererrer'><b>" + path[1] + "</b>." + path[2] + "<span class='" + hiddens[i].name + "'></span></div></a>");
 		}
 		div.innerHTML = lines.join("\n");
 		div.className = "ajax_referrers_popup small";
 		$(this).after(div);
 		this.popup = div;
+	}
+	
+	$('.ajax_referrers').click(function() {
+		trigger.apply(this);
+	});
+	
+	$('.ajax_referrers_opened').each(function() {
+		trigger.apply(this);
 	});
 });
